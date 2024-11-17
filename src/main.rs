e216ebfb0fc1ada0198105353f14a44361801424
macydnah <juande.278@gmail.com>
2024-11-17 01:29:30 -0600

    feat: add basic handler functionality

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use pulsectl::controllers::DeviceControl;
+use pulsectl::controllers::SinkController;
+
 fn dec_vol() {
 }
 
-fn inv_vol() {
+fn inc_vol(mut handler: SinkController, device_index: u32) {
+    handler.increase_device_volume_by_percent(device_index, 0.05);
 }
 
 fn mute_unmute() {
 }
 
 fn main() {
-    println!("Hello, world!");
+    // create handler that calls functions on playback devices and apps
+    let handler = SinkController::create().unwrap();
+
+    let device_index = 0;
+
+    inc_vol(handler, device_index);
 }
