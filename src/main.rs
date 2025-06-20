5461c172f8d6ce9dc08d5e1b4a27bca1b098a47f
macydnah <86342863+macydnah@users.noreply.github.com>
2025-06-19 18:54:57 -0600

    fix: return `Some(DEFAULT_VOLUME)` if pre_vol not found

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,16 @@ use pulsectl::controllers::SinkController;
 use pulsectl::controllers::types::DeviceInfo;
 use std::{env, fs, thread, time};
 
+const DEFAULT_VOLUME: Volume = Volume(65536 / 4); // 25% volume
+const DEFAULT_INCREMENT: f64 = 4.9;
+const DEFAULT_DECREMENT: f64 = 6.0;
+
 const INC_STEPS: u8 = 10;
 const DEC_STEPS: u8 = 10;
-const INC_PERCENT: f64 = 4.9;
-const DEC_PERCENT: f64 = 6.0;
-const FADE_IN_PERCENT_PER_STEP: f64 = 9.0;
-const FADE_OUT_PERCENT_PER_STEP: f64 = 20.0;
+
+const FADE_IN_INCREMENT_PER_STEP: f64 = 9.0;
+const FADE_OUT_DECREMENT_PER_STEP: f64 = 20.0;
+
 const WAIT_BETWEEN_STEPS: time::Duration = time::Duration::from_millis(26);
 
 fn get_current_vol(handler: &mut SinkController) -> Volume {
@@ -43,7 +47,7 @@ fn get_current_vol(handler: &mut SinkController) -> Volume {
 
 enum VolDataCommand {
     Query,
-    Save
+    SaveAsPreviousVolume
 }
 
 type CurrentVolume = VolDataCommand;
@@ -54,30 +58,31 @@ fn vol_data(handler: &mut SinkController, cmd: VolDataCommand) -> Option<Volume>
     match cmd {
         PreviousVolume::Query => {
             // read previous volume from file
-            let file = fs::read(file)
-                .expect("Failed to read pre_vol file");
-
-            let vol = u32::from_le_bytes(
-                file
-                .try_into()
-                .expect("Failed to convert file to u32")
-            );
-
-            return Some(Volume(vol))
-        }
-        CurrentVolume::Save => {
+            match fs::read(file) {
+                Ok(data) => {
+                    let vol = u32::from_le_bytes(
+                        data
+                        .try_into()
+                        .expect("Failed to convert file to u32")
+                    );
+                    return Some(Volume(vol));
+                },
+                Err(_) => return Some(DEFAULT_VOLUME)
+            };
+        },
+        CurrentVolume::SaveAsPreviousVolume => {
             // save current volume to file
             let vol = get_current_vol(handler).0;
             fs::write(file, vol.to_le_bytes())
                 .expect("Unable to write pre_vol file");
 
             return None
-        }
-    }
+        },
+    };
 }
 
-fn inc_vol(handler: &mut SinkController, dev_idx: u32, inc_percent: f64, target_volume: Option<Volume>) {
-    let inc_percent_per_step = inc_percent / 1000.0;
+fn inc_vol(handler: &mut SinkController, dev_idx: u32, increment: f64, target_volume: Option<Volume>) {
+    let inc_percent_per_step: f64 = (increment / 100.0) / INC_STEPS as f64;
 
     // crescendo
     handler.set_device_mute_by_index(dev_idx, false);
@@ -95,8 +100,8 @@ fn inc_vol(handler: &mut SinkController, dev_idx: u32, inc_percent: f64, target_
     };
 }
 
-fn dec_vol(handler: &mut SinkController, dev_idx: u32, dec_percent: f64) {
-    let dec_percent_per_step = dec_percent / 1000.0;
+fn dec_vol(handler: &mut SinkController, dev_idx: u32, decrement: f64) {
+    let dec_percent_per_step: f64 = (decrement / 100.0) / DEC_STEPS as f64;
 
     // diminuendo
     let mut i = 0;
@@ -107,36 +112,37 @@ fn dec_vol(handler: &mut SinkController, dev_idx: u32, dec_percent: f64) {
     };
 }
 
-fn mute(handler: &mut SinkController, dev_idx: u32, fade_out_percent_per_step: f64) {
-    // save current volume as a previous volume in case we want to fade in later
-    // don't overwrite the previous volume with zero, i don't hear unmuting to silence
-    if !get_current_vol(handler).eq(&Volume::MUTED) {
-        vol_data(handler, CurrentVolume::Save);
+fn mute(handler: &mut SinkController, dev_idx: u32, decrement_per_step: f64) {
+    if get_current_vol(handler).eq(&Volume::MUTED) {
+        return;
     };
+
+    // save current volume before fading out just in case we want to fade in later
+    vol_data(handler, CurrentVolume::SaveAsPreviousVolume);
 
     // fade out
     while get_current_vol(handler).gt(&Volume::MUTED) {
-        dec_vol(handler, dev_idx, fade_out_percent_per_step);
+        dec_vol(handler, dev_idx, decrement_per_step);
     };
     handler.set_device_mute_by_index(dev_idx, true);
 }
 
-fn unmute(handler: &mut SinkController, dev_idx: u32, fade_in_percent_per_step: f64) {
+fn unmute(handler: &mut SinkController, dev_idx: u32, increment_per_step: f64) {
     // set target volume to previously saved volume
     let target_volume: Volume = vol_data(handler, PreviousVolume::Query).unwrap();
 
     // fade in
     handler.set_device_mute_by_index(dev_idx, false);
     while get_current_vol(handler).lt(&target_volume) {
-        inc_vol(handler, dev_idx, fade_in_percent_per_step, Some(target_volume));
+        inc_vol(handler, dev_idx, increment_per_step, Some(target_volume));
     };
 }
 
 fn toggle_mute(handler: &mut SinkController, dev_idx: u32) {
     if get_current_vol(handler).gt(&Volume::MUTED) {
-        mute(handler, dev_idx, FADE_OUT_PERCENT_PER_STEP);
+        mute(handler, dev_idx, FADE_OUT_DECREMENT_PER_STEP);
     } else {
-        unmute(handler, dev_idx, FADE_IN_PERCENT_PER_STEP);
+        unmute(handler, dev_idx, FADE_IN_INCREMENT_PER_STEP);
     };
 }
 
@@ -159,34 +165,34 @@ enum Dynamics {
     #[command(arg_required_else_help = false, alias = "i", alias = "inc")]
     // #[command(visible_alias = "i", alias = "inc")]
     Increase {
-        /// volume percentage to increment
-        #[arg(value_name = "INCREMENT", default_value_t = INC_PERCENT)]
-        inc_percent: f64,
+        /// specify percent to increase
+        #[arg(value_name = "VOL", default_value_t = DEFAULT_INCREMENT)]
+        increment: f64,
         // wait_between_steps: Option<f64>,
     },
 
     /// decrease volume in diminuendo
     #[command(alias = "d", alias = "dec")]
     Decrease {
-        /// volume percentage to decrement
-        #[arg(value_name = "DECREMENT", default_value_t = DEC_PERCENT)]
-        dec_percent: f64,
+        /// specify percent to decrease
+        #[arg(value_name = "VOL", default_value_t = DEFAULT_DECREMENT)]
+        decrement: f64,
     },
 
     /// al niente (fade out to mute)
     #[command(alias = "m")]
     Mute {
-        /// decrement volume by a percentage step
-        #[arg(value_name = "DECREMENT", default_value_t = FADE_OUT_PERCENT_PER_STEP)]
-        fade_out_percent_per_step: f64,
+        /// how much volume percent to decrease per step
+        #[arg(default_value_t = FADE_OUT_DECREMENT_PER_STEP)]
+        decrement_per_step: f64,
     },
 
     /// dal niente (fade in from mute)
     #[command(alias = "u")]
     Unmute {
-        /// increment volume by a percentage step
-        #[arg(value_name = "INCREMENT", default_value_t = FADE_IN_PERCENT_PER_STEP)]
-        fade_in_percent_per_step: f64,
+        /// how much volume percent to increase per step
+        #[arg(default_value_t = FADE_IN_INCREMENT_PER_STEP)]
+        increment_per_step: f64,
     },
 
     /// toggle al niente/dal niente
@@ -208,21 +214,21 @@ fn main() {
 
     match args.dynamics {
         // Dynamics::Increase => {
-        Dynamics::Increase { inc_percent } => {
+        Dynamics::Increase { increment } => {
             print!("Crescendo\n");
-            inc_vol(&mut handler, device.index, inc_percent, None);
+            inc_vol(&mut handler, device.index, increment, None);
         }
-        Dynamics::Decrease { dec_percent } => {
+        Dynamics::Decrease { decrement } => {
             print!("Diminuendo\n");
-            dec_vol(&mut handler, device.index, dec_percent);
+            dec_vol(&mut handler, device.index, decrement);
         }
-        Dynamics::Mute { fade_out_percent_per_step } => {
+        Dynamics::Mute { decrement_per_step } => {
             print!("Diminuendo al niente\n");
-            mute(&mut handler, device.index, fade_out_percent_per_step);
+            mute(&mut handler, device.index, decrement_per_step);
         }
-        Dynamics::Unmute { fade_in_percent_per_step } => {
+        Dynamics::Unmute { increment_per_step } => {
             print!("Crescendo dal niente\n");
-            unmute(&mut handler, device.index, fade_in_percent_per_step);
+            unmute(&mut handler, device.index, increment_per_step);
         }
         Dynamics::ToggleMute => {
             print!("Toggled mute state\n");
